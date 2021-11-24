f005537642286ccf60fad5061bf904cfa0f78b79
weips <weips001@163.com>
2021-11-24 23:39:12 +0800

    增加跳转

--- pages/payPage/index.js
+++ pages/payPage/index.js
@@ -1,6 +1,3 @@
-
-const { setUserStorage } = require('../../utils/storage')
-
 Page({
   data: {
     kemuType: '',
@@ -8,44 +5,33 @@ Page({
   },
   getOpenid() {
     const { kemuType, from } = this.data
-    wx.redirectTo({
-      url: `/pages/result/result?kemuType=${kemuType}&from=${from}&openid=oEQlf5Aw-x8SDGcazlK_mc6SNQLI`,
-    });
-    // wx.cloud.callFunction({
-    //   name: "pay",
-    //   success(res) {
-    //     const { openid, timeStamp, nonceStr, package: pk, signType, paySign } = res.result
-    //     wx.requestPayment({
-    //       timeStamp,
-    //       nonceStr,
-    //       package: pk,
-    //       signType,
-    //       paySign,
-    //       success: res => {
-    //         //支付成功
-    //         openVip(openid).then(res => {
-    //           const { code, data } = res
-    //           if (code === 0) {
-    //             setUserStorage(data)
-    //             wx.redirectTo({
-    //               url: `/pages/test/test?kemuType=${kemuType}&from=${from}&payType=success`,
-    //             });
-    //           } else {
-
-    //           }
-    //         })
-    //       },
-    //       fail: err => {
-    //         wx.redirectTo({
-    //           url: `/pages/result/result?kemuType=${kemuType}&from=${from}&payType=error`,
-    //         });
-    //       }
-    //     })
-    //   },
-    //   fail(res) {
-    //     console.log(res, 744)
-    //   }
-    // })
+    wx.cloud.callFunction({
+      name: "pay",
+      success(res) {
+        const { openid, timeStamp, nonceStr, package: pk, signType, paySign } = res.result
+        wx.requestPayment({
+          timeStamp,
+          nonceStr,
+          package: pk,
+          signType,
+          paySign,
+          success: res => {
+            //支付成功
+            wx.redirectTo({
+              url: `/pages/result/result?kemuType=${kemuType}&from=${from}&openid=${openid}`,
+            });
+          },
+          // fail: err => {
+          //   wx.redirectTo({
+          //     url: `/pages/result/result?kemuType=${kemuType}&from=${from}&payType=error`,
+          //   });
+          // }
+        })
+      },
+      fail(res) {
+        console.log(res, 744)
+      }
+    })
   },
   onLoad(options) {
     const { kemuType, from } = options

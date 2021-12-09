103f796b3f81bbbf921965463c09bc08c771bdb7
lisanai <lisancai511341@163.com>
2021-12-09 09:51:24 +0800

    合并代码

--- pages/kemuOne/index.js
+++ pages/kemuOne/index.js
@@ -1,57 +1,53 @@
 // pages/kemuOne/index.js
-import { getKeyFromStorage, getErrorIdLists } from '../../utils/util';
-import { getUserStorage } from '../../utils/storage'
+import { getKeyFromStorage, getErrorIdLists } from "../../utils/util";
+import { getUserStorage } from "../../utils/storage";
 const app = getApp();
-console.log(app.globalData)
+console.log(app.globalData);
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
-    openVip: false
-=======
-    showVip: false
->>>>>>> 2ea509e3dd98d99eb457b4eedb37a44f018ad765
+    showVip: false,
   },
   _gotoCollection(from) {
-    const key = 'one_COLLECTION'
+    const key = "one_COLLECTION";
     const collection = getKeyFromStorage(key) || {};
     const ids = Object.keys(collection);
     if (ids.length) {
-      this.navigateFrom(from)
-      return true
+      this.navigateFrom(from);
+      return true;
     }
     wx.showToast({
-      icon: 'none',
-      title: '暂无收藏',
+      icon: "none",
+      title: "暂无收藏",
       duration: 1000,
     });
-    return false
+    return false;
   },
   _gotoVip(from) {
-    const user = getUserStorage()
+    const user = getUserStorage();
     if (user && user.isVip) {
       wx.navigateTo({
         url: `/pages/vipDetail/index?kemuType=one&from=${from}`,
       });
-      return
+      return;
     }
     wx.navigateTo({
       url: `/pages/payPage/index?kemuType=one&from=${from}`,
     });
   },
   _gotoError(from) {
-    const key = 'one_ERROR'
-    const errNums = wx.getStorageSync(key) || {}
-    const len = Object.keys(errNums).length
+    const key = "one_ERROR";
+    const errNums = wx.getStorageSync(key) || {};
+    const len = Object.keys(errNums).length;
     if (len > 0) {
-      this.navigateFrom(from)
-      return false
+      this.navigateFrom(from);
+      return false;
     }
     wx.showToast({
-      icon: 'none',
-      title: '暂无错题',
+      icon: "none",
+      title: "暂无错题",
       duration: 1000,
     });
   },
@@ -67,8 +63,8 @@ Page({
   },
   gotoGrade() {
     wx.navigateTo({
-      url: `/pages/grade/grade?kemuType=one`
-    })
+      url: `/pages/grade/grade?kemuType=one`,
+    });
   },
   navigateFrom(from) {
     wx.navigateTo({
@@ -76,53 +72,49 @@ Page({
     });
   },
   gotoSubject: function (type) {
-    const from = type.currentTarget.id
+    const from = type.currentTarget.id;
     switch (from) {
-      case 'MOCK':
+      case "MOCK":
         this._gotoMockExam(from);
         break;
-      case 'special':
+      case "special":
         this._gotoSpecial(from);
         break;
-      case 'VIP':
+      case "VIP":
         this._gotoVip(from);
         break;
-      case 'grade':
+      case "grade":
         this.gotoGrade();
         break;
-      case 'ERROR':
-        this._gotoError(from)
-        break
-      case 'COLLECTION':
-        this._gotoCollection(from)
-        break
+      case "ERROR":
+        this._gotoError(from);
+        break;
+      case "COLLECTION":
+        this._gotoCollection(from);
+        break;
       default:
-        this.navigateFrom(from)
+        this.navigateFrom(from);
         break;
     }
   },
   showVip() {
-    console.log(123)
+    console.log(123);
   },
   /**
    * 生命周期函数--监听页面加载
    */
-<<<<<<< HEAD
-  onLoad: function (options) { 
-=======
   onLoad: function (options) {
-    console.log('one')
-    const showVip = wx.getStorageSync('showVip')
+    console.log("one");
+    const showVip = wx.getStorageSync("showVip");
     this.setData({
-      showVip
-    })
->>>>>>> 2ea509e3dd98d99eb457b4eedb37a44f018ad765
+      showVip,
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () { },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -132,26 +124,25 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () { },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () { },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () { },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () { },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () { },
-
+  onShareAppMessage: function () {},
 });
